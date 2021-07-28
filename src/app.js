bc81d235f05843f1f94e18e3396b0db8ad3f9ffa
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-07-28 08:09:42 -0600

    working app

--- src/app.js
+++ src/app.js
@@ -1,37 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-
-const app = express()
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
-
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server)
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
- })
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
- app.use(function errorHandler(error, req, res, next) {
-      let response
-      if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-      } else {
-        console.error(error)
-        response = { message: error.message, error }
-      }
-      res.status(500).json(response)
-    })
-
-
-
-module.exports = app
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
